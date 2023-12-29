6ceb3925aea8a0f206ce3220f7c228a5e8c5b0a9
Raehanwidi2002 <rehanwidisugiarto2002@gmail.com>
2023-12-30 04:55:42 +0700

    Update

--- koi_smartpool-coba/lib/app/modules/home/controllers/home_controller.dart
+++ koi_smartpool-coba/lib/app/modules/home/controllers/home_controller.dart
@@ -1,14 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-    FirebaseAuth auth = FirebaseAuth.instance;
+  String? userName;
+  FirebaseAuth auth = FirebaseAuth.instance;
 
-  Stream<User?> streamAuthStatus(){
+  Stream<User?> streamAuthStatus() {
     return auth.authStateChanges();
   }
 
-  void signin(){}
-  void login(){}
-  void logout(){}
+  void signin() {}
+  void login() {}
+  void logout() {}
 }
